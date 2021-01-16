a26409698d837f2518adf77d7d97a6b941562a58
kohei karasudani <pulinkouhei1416@gmail.com>
2021-01-16 17:01:27 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,25 +11,23 @@
             {!! Form::open(['route' => 'signup.post']) !!}
                 <div class ="form-group">
                     {!! Form::label('name','Name') !!}
-                    {!! Form::text('name',old('name'),['class' => 'form-control'])!! }
+                    {!! Form::text('name',old('name'),['class' => 'form-control']) !!}
                 </div>            
                 <div class ="form-group">
                     {!! Form::label('email','Email') !!}
-                    {!! Form::text('email',old('email'),['class' => 'form-control'])!! }
+                    {!! Form::email('email',old('email'),['class' => 'form-control']) !!}
                 </div>
                 <div class ="form-group">
                     {!! Form::label('password','Password') !!}
-                    {!! Form::text('password',old('password'),['class' => 'form-control'])!! }
+                    {!! Form::password('password',old('password'),['class' => 'form-control']) !!}
                 </div>
                 <div class ="form-group">
                     {!! Form::label('password_confirmation','Confirmation') !!}
-                    {!! Form::text('password_confirmation',['class' => 'form-control'])!! }
+                    {!! Form::password('password_confirmation',['class' => 'form-control']) !!}
                 </div>
             
             {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
         </div>
-        
     </div>
-    
 @endsection
